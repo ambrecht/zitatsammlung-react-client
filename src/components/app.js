9b909aeebcd374de7ecfa4ae4c8be5d543852871
ambrecht <tino@ambrecht.de>
2020-12-10 20:05:43 +0100

    Header + Logo + Searchbar

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogoHeader from './header'
+import SearchBar from './SearchBar'
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <LogoHeader />
+            <SearchBar />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+// src/Title.js
+
+import React from 'react'
+import logo from '../files/logo.png'
+
+function LogoHeader() {
+    return (
+        <div>
+            <img className="ui centered medium image" src={logo}></img>
+        </div>
+    )
+}
+
+export default LogoHeader
