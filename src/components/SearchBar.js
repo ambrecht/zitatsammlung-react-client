84f05d9d2fffe7d8a37c9e420da9621e28887618
ambrecht <tino@ambrecht.de>
2020-12-11 16:38:01 +0100

    Implement AXIOS

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
 class SearchBar extends React.Component {
     state = { term: '' }
 
+    onFormSubmit = (event) => {
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.term)
+    }
+
     render() {
         return (
             <div>
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <input
                             type="text"
--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,19 @@ import React from 'react'
 import LogoHeader from './header'
 import SearchBar from './SearchBar'
 
-const App = () => {
-    return (
-        <div className="ui container">
-            <LogoHeader />
-            <SearchBar />
-        </div>
-    )
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term)
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <LogoHeader />
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
 }
 
 export default App
